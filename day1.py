b3af8fca7e3135e1ef9be0765a639af7fc19abf8
huyhui6666 <44888897+huyhui6666@users.noreply.github.com>
2019-01-26 15:54:34 +0800

    作业

---
+++ day1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 26 15:27:59 2019
+
+@author: huyhui
+"""
+name = input("输入姓名：")
+age = input("输入年龄：")
+sex = input("输入性别：")
+print('您的姓名是：%s，您的性别是：%s，您是%d年出生的人'%(name,sex,2019-int(age)))
